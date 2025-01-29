dea9723c9f3b30b72444ce886c1d80ef4b57627c
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2025-01-29 20:41:15 +0200

    initial-commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'blank',
+    loadComponent: () => import('./layouts/blank-layout/blank-layout.component').then(m => m.BlankLayoutComponent),
+    children: [
+      { path: '', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent), title: 'Home' },
+      { path: 'cart', loadComponent: () => import('./components/cart/cart.component').then(m => m.CartComponent), title: 'Cart' },
+      { path: 'login', loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent), title: 'Login' },
+      { path: 'register', loadComponent: () => import('./components/register/register.component').then(m => m.RegisterComponent), title: 'Register' },
+      { path: 'brands', loadComponent: () => import('./components/brands/brands.component').then(m => m.BrandsComponent), title: 'Brands' },
+      { path: 'categories', loadComponent: () => import('./components/categories/categories.component').then(m => m.CategoriesComponent), title: 'Categories' },
+      { path: 'products', loadComponent: () => import('./components/products/products.component').then(m => m.ProductsComponent), title: 'Products' },
+      { path: 'not-found', loadComponent: () => import('./components/notfound/notfound.component').then(m => m.NotfoundComponent), title: 'Not Found' },
+      { path: '**', redirectTo: 'not-found', pathMatch: 'full' }
+    ],
+  },
+  {
+    path: 'auth',
+    loadComponent: () => import('./layouts/auth-layout/auth-layout.component').then(m => m.AuthLayoutComponent),
+    children: [
+      { path: '', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent), title: 'Home' },
+      { path: 'login', loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent), title: 'Login' },
+      { path: 'register', loadComponent: () => import('./components/register/register.component').then(m => m.RegisterComponent), title: 'Register' },
+      { path: 'not-found', loadComponent: () => import('./components/notfound/notfound.component').then(m => m.NotfoundComponent), title: 'Not Found' },
+      { path: '**', redirectTo: 'not-found', pathMatch: 'full' }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/layouts/blank-layout/blank-layout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-blank-layout',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './blank-layout.component.html',
+  styleUrls: ['./blank-layout.component.css']
+})
+export class BlankLayoutComponent {
+
+}
